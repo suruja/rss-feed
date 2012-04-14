1cc1a52d236ce01d0f8b3c179b5a21742fb973e5
Sebastien Azimi <sebstp@gmail.com>
2012-04-14 03:31:55 +0200

    New contextual and more complex specs added.

--- spec/rss-feed_spec.rb
+++ spec/rss-feed_spec.rb
@@ -14,13 +14,31 @@ describe Rss::Feed do
     model.destroy_all
   end
 
-  it ".get_feed_from" do
-    model.get_feed_from(feed_url)
-    model.feed_entries.should_not be_empty
+  describe ".get_feed_from" do
+
+    context "when a valid feed url is provided" do
+      it "gets feed entries from the feed url" do
+        model.get_feed_from(feed_url)
+        model.feed_entries.should_not be_empty
+      end
+    end
+
+    context "when an invalid or empty feed url is provided" do
+      it "does not get anything" do
+        model.get_feed_from('')
+        model.feed_entries.should be_empty
+      end
+    end
   end
 
-  it ".update_from_feed" do
-    model.update_from_feed
-    model.count.should equal model.feed_entries.count
+  describe ".update_from_feed" do
+
+    it "insert new documents for each unexistant feed entry" do
+      model.update_from_feed
+      model.count.should equal model.feed_entries.count
+
+      model.update_from_feed
+      model.count.should equal model.feed_entries.count
+    end
   end
 end
